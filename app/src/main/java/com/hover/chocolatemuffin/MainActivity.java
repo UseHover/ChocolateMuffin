c0ad5e30c649c8bdebd48845b6a0978c18d79590
kutalek <davkutalek@gmail.com>
2017-03-06 12:43:54 +0630

    Updated to 0.12.x API (new operator onboarding)

--- app/src/main/java/com/hover/chocolatemuffin/MainActivity.java
+++ app/src/main/java/com/hover/chocolatemuffin/MainActivity.java
@@ -16,12 +16,11 @@ import android.widget.Toast;
 
 import com.hover.sdk.buttons.BuyButton;
 import com.hover.sdk.buttons.BuyButtonCallback;
-import com.hover.sdk.main.HoverIntegration;
+import com.hover.sdk.onboarding.HoverIntegrationActivity;
 import com.hover.sdk.main.HoverParameters;
-import com.hover.sdk.main.PermActivity;
 import com.hover.sdk.operators.Permission;
 
-public class MainActivity extends AppCompatActivity implements HoverIntegration.HoverListener, AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 	public static String TAG = "Main Activity";
 	public static final int PERM_REQUEST = 0, BUY_REQUEST = 1;
 
@@ -29,7 +28,7 @@ public class MainActivity extends AppCompatActivity implements HoverIntegration.
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		startActivityForResult(new Intent(this, PermActivity.class), PERM_REQUEST);
+		addHoverIntegration();
 		fillViews();
 	}
 
@@ -40,23 +39,12 @@ public class MainActivity extends AppCompatActivity implements HoverIntegration.
 		}
 	}
 
-	private void addHoverIntegration() { HoverIntegration.add(new int[] {3, 4, 5, 6, 7, 8, 11, 14, 15, 16, 13, 17, 19, 20}, Permission.NORMAL, this, this); }
-
-	@Override public void onSIMError(String message) { Log.d(TAG, "Sim error: " + message); }
-	@Override public void onError(String message) {
-		Log.d(TAG, "Error: " + message);
-	}
-	@Override public void onUserDenied() { Log.d(TAG, "User denied"); }
-	@Override
-	public void onSuccess(int serviceId, String serviceName, String opSlug, String countryName, String currency) {
-		Log.d(TAG, "Successfully added service: " + serviceName);
-		Utils.setCurrency(currency, MainActivity.this);
-		Utils.setServiceId(serviceId, MainActivity.this);
-		Utils.setServiceName(serviceName, MainActivity.this);
-		((TextView) findViewById(R.id.currency)).setText(currency);
-
-		updateParams();
-		((BuyButton) findViewById(R.id.hover_button)).setCallback(mBtnCallbacks);
+	private void addHoverIntegration() {
+		Intent integrationIntent = new Intent(this, HoverIntegrationActivity.class);
+		integrationIntent.putExtra(HoverIntegrationActivity.SERVICE_IDS, new int[] { 8 });
+		integrationIntent.putExtra(HoverIntegrationActivity.PERM_LEVEL, Permission.NORMAL);
+		startActivityForResult(integrationIntent, PERM_REQUEST);
+//		HoverIntegration.add(new int[] {3, 4, 5, 6, 7, 8, 11, 14, 15, 16, 13, 17, 19, 20}, Permission.NORMAL, this, this);
 	}
 
 	private void updateParams() {
@@ -67,12 +55,12 @@ public class MainActivity extends AppCompatActivity implements HoverIntegration.
 			default: request = "send";
 		}
 		BuyButton btn = ((BuyButton) findViewById(R.id.hover_button));
-		btn.setText(getString(R.string.buy_with, Utils.getServiceName(this)));
+		btn.setText(getString(R.string.buy_with, "Wave Money"));
 		btn.setBuyParameters(
 				new HoverParameters.Builder(this)
 						.request(request, Utils.getPrice(this), Utils.getCurrency(this), Utils.getRecip(this))
 						.from(Utils.getServiceId(this))
-						.build());
+						.build(), BUY_REQUEST);
 	}
 
 	private void updateRunningTotal() {
@@ -85,14 +73,27 @@ public class MainActivity extends AppCompatActivity implements HoverIntegration.
 	@Override
 	protected void onActivityResult (int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		Log.d(TAG, "Received result");
+		Log.e(TAG, "Received result");
 		if (requestCode == PERM_REQUEST && resultCode == RESULT_OK)
-			addHoverIntegration();
+			onSuccess(data);
 		else if (requestCode == PERM_REQUEST)
-			onUserDenied();
+			onUserCanceled();
 		else if (requestCode == BUY_REQUEST)
 			((BuyButton) findViewById(R.id.hover_button)).onActivityResult(requestCode, resultCode, data);
 	}
+
+	public void onSuccess(Intent data) {
+		Log.e(TAG, "Successfully added service: " + data.getStringExtra("serviceName"));
+		Utils.setCurrency(data.getStringExtra("currency"), this);
+		Utils.setServiceId(data.getIntExtra("serviceId", -1), this);
+		Utils.setServiceName(data.getStringExtra("serviceName"), this);
+		((TextView) findViewById(R.id.currency)).setText(data.getStringExtra("currency"));
+
+		updateParams();
+		((BuyButton) findViewById(R.id.hover_button)).setCallback(mBtnCallbacks);
+	}
+
+	public void onUserCanceled() { Log.d(TAG, "User canceled"); }
 
 	private BuyButtonCallback mBtnCallbacks = new BuyButtonCallback() {
 		@Override public void onError(Throwable throwable) {
@@ -109,10 +110,6 @@ public class MainActivity extends AppCompatActivity implements HoverIntegration.
 
 		@Override public void onServiceProcessing(String s, int i) {
 			Toast.makeText(MainActivity.this, "Processing...\n " + s, Toast.LENGTH_LONG).show();
-		}
-
-		@Override public void onSuccess(String s, int i) {
-
 		}
 	};
 
