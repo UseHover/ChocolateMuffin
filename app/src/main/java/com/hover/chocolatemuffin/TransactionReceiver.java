de8d6fea7f1d35ae5441c119d0f9f5ff695924c6
kutalek <davkutalek@gmail.com>
2019-04-03 14:49:50 -0700

    Updated Chocolate Muffin to latest SDK

--- app/src/main/java/com/hover/chocolatemuffin/TransactionReceiver.java
+++ app/src/main/java/com/hover/chocolatemuffin/TransactionReceiver.java
@@ -5,15 +5,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class TransactionReceiver extends BroadcastReceiver {
 	public TransactionReceiver() { }
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		try {
-			if (intent.hasExtra("amount"))
-				Utils.increaseTotalSpent(Float.valueOf(intent.getStringExtra("amount")).intValue(), context);
 			Utils.increaseMuffinCount(context);
+			if (intent.hasExtra("transaction_extras")) {
+				//noinspection unchecked
+				String amount = ((HashMap<String, String>) intent.getExtras().get("transaction_extras")).get("amount");
+				Utils.increaseTotalSpent(Float.valueOf(amount).intValue(), context);
+			}
 //			Toast.makeText(context, context.getString(R.string.successfully_bought, intent.getStringExtra("amount")), Toast.LENGTH_LONG).show();
 		} catch (Exception e) {
 			Toast.makeText(context, context.getString(R.string.error_receiver), Toast.LENGTH_LONG).show();
